00556f018fde5862381b1e690ca8e6d3bd338f8e
Lusca Andrei <oscicen@gmail.com>
2023-06-23 20:20:06 +0300

    refactor to show all questions equaly

--- app/page.js
+++ app/page.js
@@ -16,79 +16,70 @@ import {
 } from '@chakra-ui/react'
 
 import Question from '@/components/Question'
-import CategoryStats from '@/components/CategoryStats'
+import shuffle from '@/lib/shuffleArray'
 import { data } from '@/db/data'
 
 import styles from './page.module.scss'
 
 export default function Home() {
-  const [categoryId, setCategoryId] = useState(2);
-  const [questionId, setQuestionId] = useState(55);
+  const [categoryId, setCategoryId] = useState(0);
+  const [questionsId, setQuestionsId] = useState([]);
   const [category, setCategory] = useState(null);
   const [question, setQuestion] = useState(null);
-  const [filters, setFilters] = useState([]);
   const [isRandom, setIsRandom] = useState(false);
   const [open, setOpen] = useState(false);
   const [stats, setStats] = useState({ answered: 0, correct: 0 });
 
   useEffect(() => {
     if (Cookies.get('stats')) {
-      setStats(JSON.parse(Cookies.get('stats')));
+      setStats(JSON.parse(Cookies.get('stats')))
     } else {
-      Cookies.set('stats', JSON.stringify({ answered: 0, correct: 0 }), { expires: 7 });
+      Cookies.set('stats', JSON.stringify({ answered: 0, correct: 0 }), { expires: 7 })
     }
   }, []);
 
   useEffect(() => {
-    const newCategory = data.find(c => c.id === categoryId);
-    const newQuestion = newCategory.questions.find(q => q.id === questionId);
-    setCategory(newCategory);
-    setQuestion(newQuestion);
-  }, [categoryId, questionId])
+    generateQuestionsId()
+  }, [categoryId])
 
   useEffect(() => {
-    if (isRandom) {
-      randomQuestion()
-    } else {
-      setCategoryId(filters.length ? filters[0] : 0);
-      setQuestionId(0)
+    if (questionsId.length) {
+      const updatedQuestionsId = [...questionsId]
+      setQuestionsId(isRandom ? shuffle(updatedQuestionsId) : updatedQuestionsId.sort((a, b) => a - b))
     }
-  }, [filters])
+  }, [isRandom])
 
-  const randomQuestion = () => {
-    const noOfCategories = filters.length || data.length;
-    const randomCategory = Math.floor(Math.random() * (noOfCategories - 0) + 0);
-    const noOfQuestions = filters.length ? data.find(c => c.id === filters[randomCategory]).questions.length : data[randomCategory].questions.length;
-    const randomQuestion = Math.floor(Math.random() * (noOfQuestions - 0) + 0);
-    setQuestionId(randomQuestion);
-    setCategoryId(filters.length ? filters[randomCategory] : randomCategory);
+  useEffect(() => {
+    const newCategory = data.find(c => c.id === categoryId);
+    const newQuestion = newCategory.questions.find(q => q.id === questionsId[0])
+    setCategory(newCategory)
+    setQuestion(newQuestion)
+  }, [categoryId, questionsId])
+
+  const generateQuestionsId = () => {
+    const ids = Array.from(Array(data[categoryId].questions.length).keys())
+    setQuestionsId(isRandom ? shuffle(ids) : ids)
   }
 
-  const nextQuestion = (filter) => {
-    if (data[categoryId].questions[questionId + 1]) {
-      setQuestionId(questionId + 1)
-    } else if (data[categoryId + 1]) {
-      setCategoryId(categoryId + 1)
-      setQuestionId(0)
+  const nextQuestion = () => {
+    if (questionsId.length > 1) {
+      const updatedQuestionsId = [...questionsId]
+      updatedQuestionsId.shift()
+      setQuestionsId(updatedQuestionsId)
     } else {
-      setCategoryId(0)
-      setQuestionId(0)
+      generateQuestionsId()
     }
   }
 
-  const handleFilter = (filterId) => {
-    let newFilters;
-    if (filters.includes(filterId)) {
-      newFilters = [...filters].filter(f => f !== filterId)
-    } else {
-      newFilters = [...filters, filterId]
+  const handleCategoryChange = (selectedCategory) => {
+    if (categoryId !== selectedCategory) {
+      setCategoryId(selectedCategory)
     }
-    setFilters(newFilters.sort((a ,b) => a - b));
   }
 
   const resetStats = () => {
-    setStats({ answered: 0, correct: 0 });
-    Cookies.set('stats', JSON.stringify({ answered: 0, correct: 0 }), { expires: 7 });
+    setStats({ answered: 0, correct: 0 })
+    Cookies.set('stats', JSON.stringify({ answered: 0, correct: 0 }))
   }
 
   const onNext = (correct) => {
@@ -100,9 +91,9 @@ export default function Home() {
     }
 
     setStats({ ...newStats })
-    Cookies.set('stats', JSON.stringify({ ...newStats }), { expires: 7 });
+    Cookies.set('stats', JSON.stringify({ ...newStats }))
 
-    isRandom ? randomQuestion() : nextQuestion()
+    nextQuestion()
   }
 
   return (
@@ -126,13 +117,13 @@ export default function Home() {
               <p>Selecteaza categoria:</p>
               { data.map(c => (
                 <Box
-                  className={classNames({ [styles.active]: filters.includes(c.id) })}
+                  className={classNames({ [styles.active]: categoryId === c.id })}
                   key={c.id}
                   as='button'
                   borderRadius='md'
                   borderWidth='1px'
                   p={2}
-                  onClick={() => handleFilter(c.id)}
+                  onClick={() => handleCategoryChange(c.id)}
                 >
                   <h4>{ c.category }</h4>
                 </Box>
